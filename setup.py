5474579e90fe77a1871138e74ee6a03128def162
Nando Florestan <nandoflorestan@gmail.com>
2013-07-31 22:39:52 -0300

    Bump version after release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     'some software.',
     long_description=content_of('README.rst', 'CHANGES.rst'),
     url='https://github.com/nandoflorestan/releaser',
-    version='0.1.1',
+    version='0.1.2dev',
     license='MIT',
     author='Nando Florestan',
     author_email="nandoflorestan@gmail.com",
