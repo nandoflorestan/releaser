fcb9331afb7b3fb8f1e921370e8c1ceeb1b6ac9b
Nando Florestan <nandoflorestan@gmail.com>
2015-02-05 11:23:39 -0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     'some software.',
     long_description=content_of('README.rst', 'CHANGES.rst'),
     url='https://github.com/nandoflorestan/releaser',
-    version='0.1.5dev',
+    version='0.2',
     license='MIT',
     author='Nando Florestan',
     author_email="nandoflorestan@gmail.com",
