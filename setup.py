7c8481bde702e289ef68ec65f167824c125190be
Nando Florestan <nandoflorestan@gmail.com>
2016-02-24 18:05:42 -0300

    Bump version to 0.4.dev1 after release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     'some software.',
     long_description=content_of('README.rst', 'CHANGES.rst'),
     url='https://github.com/nandoflorestan/releaser',
-    version='0.3',
+    version='0.4.dev1',
     license='MIT',
     author='Nando Florestan',
     author_email="nandoflorestan@gmail.com",
