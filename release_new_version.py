4f6f3611749c5bf00ae7dd7dde9749833782690a
Nando Florestan <nandoflorestan@gmail.com>
2021-06-26 16:25:37 +0200

    Just format the releaser script with black

--- release_new_version.py
+++ release_new_version.py
@@ -2,51 +2,49 @@
 
 """Script that releases a new version of the software."""
 
-from releaser import Releaser          # easy_install -UZ releaser
+from releaser import Releaser  # easy_install -UZ releaser
 from releaser.steps import *
 from releaser.git_steps import *
 
 # These settings are used by multiple release steps below.
 config = dict(
-    github_user='nandoflorestan',  # TODO infer from .git/config
-    github_repository='releaser',  # TODO autodiscover this too
-    branch='master',  # Only release new versions in this git branch
-    changes_file='CHANGES.rst',
-    version_file='setup.py',  # Read and write version number on this file
-    version_keyword='version',    # Part of the variable name in that file
-    log_file='release.log.utf-8.tmp',
-    verbosity='info',  # debug | info | warn | error
+    github_user="nandoflorestan",  # TODO infer from .git/config
+    github_repository="releaser",  # TODO autodiscover this too
+    branch="master",  # Only release new versions in this git branch
+    changes_file="CHANGES.rst",
+    version_file="setup.py",  # Read and write version number on this file
+    version_keyword="version",  # Part of the variable name in that file
+    log_file="release.log.utf-8.tmp",
+    verbosity="info",  # debug | info | warn | error
 )
 
 # You can customize your release process below.
 # Comment out any steps you don't desire and add your own steps.
 Releaser(
     config,
-
     # ==================  Before releasing, do some checks  ===================
-    Shell('python setup.py test'),  # First of all ensure tests pass
+    Shell("python setup.py test"),  # First of all ensure tests pass
     CheckRstFiles,  # Documentation: recursively verify ALL .rst files, or:
     # CheckRstFiles('README.rst', 'CHANGES.rst', 'LICENSE.rst'),  # just a few.
     # TODO IMPLEMENT CompileAndVerifyTranslations,
-    EnsureGitClean,   # There are no uncommitted changes in tracked files.
+    EnsureGitClean,  # There are no uncommitted changes in tracked files.
     EnsureGitBranch,  # I must be in the branch specified in config
-    InteractivelyEnsureChangesDocumented,     # Did you update CHANGES.rst?
+    InteractivelyEnsureChangesDocumented,  # Did you update CHANGES.rst?
     # Shell('./build_sphinx_documentation.sh'),  # You can write it easily
     # CheckTravis,  # We run this late, so travis-ci has more time to build
-
     # ======================  All checks pass. RELEASE!  ======================
     SetVersionNumberInteractively,  # Ask for version and write to source code
     GitCommitVersionNumber,
     GitTag,  # Locally tag the current commit with the new version number
     InteractivelyApproveDistribution,  # Generate sdist, let user verify it
-    InteractivelyApproveWheel,         # Generate wheel, let user verify it
-    PypiUpload,   # Make and upload a source .tar.gz to https://pypi.org
+    InteractivelyApproveWheel,  # Generate wheel, let user verify it
+    PypiUpload,  # Make and upload a source .tar.gz to https://pypi.org
     PypiUploadWheel,  # Make and upload source wheel to https://pypi.org
-
     # ===========  Post-release: set development version and push  ============
     SetFutureVersion,  # Writes incremented version, now with 'dev' suffix
-    GitCommitVersionNumber('future_version',
-                           msg='Bump version to {0} after release'),
+    GitCommitVersionNumber(
+        "future_version", msg="Bump version to {0} after release"
+    ),
     # ErrorStep,  # You can use this step while testing - it causes a rollback.
     GitPush,  # Cannot be undone. If successful, previous steps won't roll back
     GitPushTags,
